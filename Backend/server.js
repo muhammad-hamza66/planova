667b791239b0452bdaeb866a4d8a680506131ecf
muhammad-hamza66 <muhammadhamzu12@gmail.com>
2026-06-07 14:27:20 +0400

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -39,3 +39,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+
+
+module.exports = app;
