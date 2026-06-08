d6eb0ce86d1a07e5e4ce9f36abb73fafb226519a
muhammad-hamza66 <muhammadhamzu12@gmail.com>
2026-06-09 00:26:34 +0400

    updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,30 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+
 import { Toaster } from "react-hot-toast";
+
+// Admin Pages
 import Dashboard from "./pages/Admin/Dashboard";
-import Login from "./pages/Auth/Login";
-import SignUp from "./pages/Auth/SignUp";
 import ManageTasks from "./pages/Admin/ManageTasks";
 import CreateTask from "./pages/Admin/CreateTask";
 import ManageUsers from "./pages/Admin/ManageUsers";
 
+// Auth Pages (FIXED HERE)
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+
+// User Pages
 import UserDashboard from "./pages/User/UserDashboard";
 import MyTasks from "./pages/User/MyTasks";
 import ViewTaskdetails from "./pages/User/ViewTaskdetails";
 
+// Routes & Context
 import PrivateRoute from "./pages/routes/PrivateRoute";
 import UserProvider, { UserContext } from "./context/userContext";
 
@@ -24,18 +37,25 @@ const Root = () => {
     return <Navigate to="/login" />;
   }
 
-  return user.role === "admin" ? <Navigate to="/admin/dashboard" /> : <Navigate to="/user/dashboard" />;
+  return user.role === "admin" ? (
+    <Navigate to="/admin/dashboard" />
+  ) : (
+    <Navigate to="/user/dashboard" />
+  );
 };
 
 const App = () => {
   return (
-     <UserProvider>
-    <div>
+    <UserProvider>
       <Router>
         <Routes>
+          {/* Auth Routes */}
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          
+          <Route path="/signup" element={<Signup />} />
+
+          {/* Default Route */}
+          <Route path="/" element={<Root />} />
+
           {/* Admin Routes */}
           <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
             <Route path="/admin/dashboard" element={<Dashboard />} />
@@ -44,31 +64,28 @@ const App = () => {
             <Route path="/admin/users" element={<ManageUsers />} />
           </Route>
 
-          {/* user Routes */}
+          {/* User Routes */}
           <Route element={<PrivateRoute allowedRoles={["user"]} />}>
-            <Route path="/user/dashboard" element={<UserDashboard />} /> 
-            <Route path="/user/tasks" element={<MyTasks />} /> 
-            <Route path="/user/task-details/:id" 
-            element={<ViewTaskdetails />} />
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTasks />} />
+            <Route
+              path="/user/task-details/:id"
+              element={<ViewTaskdetails />}
+            />
           </Route>
-
-          {/* < default Route */}
-          <Route path="/" element={<Root />} />
-
         </Routes>
       </Router>
-    </div>
 
-    <Toaster 
-    toastOptions={{
-      className: "" ,
-      style: {
-       fontSize: "13px" ,
-    },
-    }}
-    />
+      {/* Toast Notifications */}
+      <Toaster
+        toastOptions={{
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
     </UserProvider>
   );
 };
 
-export default App; 
+export default App;
